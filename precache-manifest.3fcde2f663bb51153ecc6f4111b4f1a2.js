95a96fb2d0825ee5ac09f95417c9fe5a18dd89c4
RoshRoslin <RoshRoslin@gmail.com>
2021-05-08 20:11:28 -0700

    Updates

---
+++ precache-manifest.3fcde2f663bb51153ecc6f4111b4f1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dff8f81ce03ac26e232f7a6e213fbc",
+    "url": "/react-slider-project/index.html"
+  },
+  {
+    "revision": "03f73fa7b6132f0c8926",
+    "url": "/react-slider-project/static/css/main.f86758df.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/react-slider-project/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-slider-project/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f73fa7b6132f0c8926",
+    "url": "/react-slider-project/static/js/main.4900c8af.chunk.js"
+  },
+  {
+    "revision": "4310ad10b4c237064b42",
+    "url": "/react-slider-project/static/js/runtime-main.c7043d0f.js"
+  }
+]);
